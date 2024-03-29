6749e3b1f34bd882d87f0f58d88325a3b1171b9d
Govermentname <163609112+ethanfernand3s@users.noreply.github.com>
2024-03-28 18:48:58 -0700

    fix

---
+++ Source/Pickleball/Private/EnemyAIController.cpp
@@ -0,0 +1,37 @@
+﻿
+#include "EnemyAIController.h"
+
+#include "BehaviorTree/BehaviorTree.h"
+#include "BehaviorTree/BehaviorTreeComponent.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+AEnemyAIController::AEnemyAIController()
+{
+	SceneComponent = CreateDefaultSubobject<USceneComponent>(TEXT("SceneComponent"));
+	SceneComponent->SetupAttachment(RootComponent);
+
+	BehaviorTreeComponent = CreateDefaultSubobject<UBehaviorTreeComponent>(TEXT("BehaviorTreeComponent"));
+	BlackboardComponent = CreateDefaultSubobject<UBlackboardComponent>(TEXT("BlackboardComponent"));
+}
+
+void AEnemyAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if(IsValid(Blackboard.Get()))
+	{
+		Blackboard->InitializeBlackboard(*BehaviorTree.Get()->BlackboardAsset.Get());
+	}
+}
+
+void AEnemyAIController::OnPossess(APawn* InPawn)
+{
+	Super::OnPossess(InPawn);
+	
+	if (BehaviorTree.Get() != nullptr)
+	{
+		BehaviorTreeComponent->StartTree(*BehaviorTree);
+	}
+
+	
+}
---
+++ Source/Pickleball/Private/PlayerPaddle.cpp
@@ -0,0 +1,94 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerPaddle.h"
+
+#include "Ball.h"
+#include "Engine.h"
+
+// Sets default values
+APlayerPaddle::APlayerPaddle()
+{
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	BoxCollider->OnComponentBeginOverlap.AddDynamic(this, &APlayerPaddle::OnPaddleBeginOverlap);
+	BoxCollider->OnComponentEndOverlap.AddDynamic(this, &APlayerPaddle::OnPaddleEndOverlap);
+	
+	bIsInHittingZone = false;
+	BallInScene = nullptr;
+}
+
+void APlayerPaddle::BeginPlay()
+{
+	Super::BeginPlay();
+	BallInScene = Cast<ABall>(UGameplayStatics::GetActorOfClass(GetWorld(), ABall::StaticClass()));
+}
+
+void APlayerPaddle::StartSwing(float ScreenYDistance, float ScreenXDistance, float SwipeTime)
+{
+	
+	// If swipe is going up
+	if((ScreenYDistance < 0) && !bIsSwingActive)
+	{
+		bIsSwingActive = true;
+
+		//Flip paddle after swipe
+		FRotator NewRotation;
+		if(!bIsFacingLeft)
+		{
+			bIsFacingLeft = true;
+			NewRotation = FRotator(0.0f, GetActorRotation().Yaw + 180.0f, 0.0f);
+		}
+		else
+		{
+			bIsFacingLeft = false;
+			NewRotation = FRotator(0.0f, GetActorRotation().Yaw - 180.0f, 0.0f);
+		}
+		
+		this->SetActorRotation(NewRotation, ETeleportType::TeleportPhysics);
+
+		GetWorld()->GetTimerManager().ClearTimer(SwingTimerHandle);
+		GetWorld()->GetTimerManager().SetTimer(SwingTimerHandle, this, &APlayerPaddle::FinishSwing, .5f, false);
+		
+		//play the sound effect
+		//play particle effect
+		
+		if(bIsInHittingZone)
+		{
+			if (IsValid(BallInScene))
+			{
+				constexpr float RelativeAdjustmentToWorld = 0.075;
+				
+				//Range of X-Y-Z Should be between 10-40
+				const float ForceXDistance = FMath::Clamp(((-ScreenYDistance * RelativeAdjustmentToWorld) / (SwipeTime * 2.5)), 20.0f, 50.0f);
+				const float ForceYDistance = FMath::Clamp(ScreenXDistance * RelativeAdjustmentToWorld, -75.0f, 75.0f);
+				const float ForceZDistance = FMath::Clamp(-ScreenYDistance * RelativeAdjustmentToWorld, 30.0f, 75.0f);
+				
+				// Normalize the swipe direction to use as a direction vector in the world
+				const FVector Force = FVector(ForceXDistance, ForceYDistance, ForceZDistance);
+				
+				// Apply the force in the calculated direction with the calculated magnitude
+				BallInScene->ApplySwipeForce(Force);
+				
+			}
+		}
+		
+	}
+}
+
+void APlayerPaddle::FinishSwing()
+{
+	bIsSwingActive = false;
+}
+
+void APlayerPaddle::OnPaddleBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+								 int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	bIsInHittingZone = true;
+}
+
+void APlayerPaddle::OnPaddleEndOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	bIsInHittingZone = false;
+}
