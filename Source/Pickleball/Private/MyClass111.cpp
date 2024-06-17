a72d103d271f7f15b3006ac51bb03afe3738e05e
TopShelfGames <topshelfgamesdev@gmail.com>
2024-06-16 19:48:53 -0700

    Packaged IOS w/ Cmd line and added git attributes
    Steps to Package Unreal Game for IOS
    
    *Make sure there is a Shipping Target Build of Game (Make Custom Scheme)*
    
    Step 0 (Open Terminal)
    
    Step 1 (Navigate to Unreal’s Command Line Build executable):
    
    	cd /Users/Shared/Epic\ Games/UE_5.3/Engine/Build/BatchFiles
    
    Step 2 (Run the Tool)
    
    	./RunUAT.sh BuildCookRun -project=/Users/topshelfgames/Documents/GitHub3/Pickleball/	Pickleball.uproject -noP4 -platform=IOS -clientconfig=Shipping -serverconfig=Shipping 	-noeditor -pak -cook -stage -archive -archivedirectory=/Users/topshelfgames/Documents/Output 	-cmdline="-allowProvisioningUpdates -BuildMachine UE_XCODE_BUILD_MODE=Default"

---
+++ Source/Pickleball/Private/MyClass111.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyClass111.h"
+
+MyClass111::MyClass111()
+{
+}
+
+MyClass111::~MyClass111()
+{
+}
---
+++ Source/Pickleball/Public/MyClass111.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class PICKLEBALL_API MyClass111
+{
+public:
+	MyClass111();
+	~MyClass111();
+};
