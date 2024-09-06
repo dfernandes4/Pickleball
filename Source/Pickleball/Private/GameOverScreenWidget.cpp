8d840d1161d9d17991842190fea06f5a74dd4a1a
dfernandes4 <124100832+dfernandes4@users.noreply.github.com>
2024-09-05 21:24:05 -0700

    Now lets comment out everything excpet for the enemy row saving

--- Source/Pickleball/Private/GameOverScreenWidget.cpp
+++ Source/Pickleball/Private/GameOverScreenWidget.cpp
@@ -173,17 +173,18 @@ void UGameOverScreenWidget::OnUserFinishedRewardAd()
     }
     else
     {
-        const TObjectPtr<UWidgetLoader> WidgetLoader = NewObject<UWidgetLoader>(this);
+        // const TObjectPtr<UWidgetLoader> WidgetLoader = NewObject<UWidgetLoader>(this);
                 
-        WidgetLoader->LoadWidget(FName("LoadingScreen"), GetWorld(), 10);
+        // WidgetLoader->LoadWidget(FName("LoadingScreen"), GetWorld(), 10);
         //PlayerPaddle->SaveLastScore();
         GameInstance->SaveCurrentEnemyRow(EnemyPaddle->GetCurrentRow());
-        
-        FTimerHandle LoadDelayHandle;
-        GetWorld()->GetTimerManager().SetTimer(LoadDelayHandle, [this]()
-        {
-        	UGameplayStatics::OpenLevel(this, FName(*GetWorld()->GetName()), false);
-        }, 1.4f, false);
+        /*
+        	FTimerHandle LoadDelayHandle;
+                GetWorld()->GetTimerManager().SetTimer(LoadDelayHandle, [this]()
+                {
+                	UGameplayStatics::OpenLevel(this, FName(*GetWorld()->GetName()), false);
+                }, 1.4f, false);
+         */
     }
 }
 
